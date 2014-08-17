71710127809c69ea3cb55a7623090bce38d88b27
Todd Pickell <tapickell@gmail.com>
2014-08-17 14:12:03 -0500

    added circumference testing on wheel

--- chapt3rev_test.rb
+++ chapt3rev_test.rb
@@ -32,12 +32,16 @@ end
 describe Wheel do
   subject { Wheel.new(rim: 26, tire: 0.7) }
 
-  describe 'it can return the correct diameter' do
+  describe 'can return the correct diameter' do
     it { subject.diameter.must_equal 27.4 }
   end
 
   describe 'can return the correct radius' do
     it { subject.radius.must_equal 13.7 }
+  end
+
+  describe 'can return the correct circumference' do
+    it { subject.circumference.must_be_close_to 86.1, 0.1 }
   end
 end
 
